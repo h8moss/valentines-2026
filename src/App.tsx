b9d900abfa80dbc1d288fa389116239f582bf5a0
h8moss <danarmenta32@gmail.com>
2026-02-08 14:10:56 -0600

    Added coupons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import React, { useState } from 'react'
+import React, { useMemo, useState } from 'react'
 import './App.css'
 import seaLion from '/sea-lion.gif'
-import heart from '/heart.svg'
+import Heart from './Heart';
+
+const numbers = (n: number): number[] => {
+  const result = [];
+  for (let i = 0; i < n; i++) {
+    result.push(i);
+  }
+
+  return result;
+}
+
+const generateSize = () => {
+  return 45 + 10 * Math.random();
+}
+
+
+function randomPick<T>(arr: T[]): T {
+  const index = rand(arr.length)
+  return arr[index];
+}
+
+const rand = (max: number) => {
+  return Math.floor(Math.random() * max)
+}
+
+function randomized<T>(list: T[]): T[] {
+  const newList = [];
+  while (list.length > 0) {
+    newList.push(list.splice(rand(list.length), 1)[0]);
+  }
+  return newList;
+}
 
 function App() {
   const [saidYes, setSaidYes] = useState(false);
@@ -14,6 +45,32 @@ function App() {
   const onNo = () => {
     setNoCounter(v => v + 1);
   }
+
+  const colors = [
+    '#ff0000',
+    '#ff0059',
+    '#ff7fab',
+    '#c40024',
+    '#9000ff',
+    '#ff00d8'
+  ]
+
+  const coupons = useMemo(() =>
+    randomized([
+      'Me coges (tu mandas)',
+      'Noche de peliculas',
+      'Cita que yo pago',
+      'Cita en la que costillitas cocina',
+      'Cosquillas infinitas (No te puedes negar)',
+      'Besitos a bees',
+      'Masaje',
+      'Acurrucadas',
+      'Vemos la serie que tu quieras'
+    ])
+
+    , [])
+
+  const [flipped, setFlipped] = useState<number[]>([]);
 
   return (
     <>
@@ -34,8 +91,27 @@ function App() {
 
         {saidYes &&
           <>
+            {numbers(50).map(i => <Heart
+              size={generateSize()}
+              color={randomPick(colors)}
+              key={i}
+              left={rand(window.innerWidth - 50)}
+              top={rand(window.innerHeight - 50)}
+              delay={rand(500)}
+            />)
+            }
+
             <h1>Yippee!!!!!!!!!!!!!!!!!!!!!</h1>
             <img id="sealion" src={seaLion} />
+
+            <div id="coupon-list">
+              {numbers(coupons.length).map(v =>
+                <div className="coupon-div" key={v} onClick={() => setFlipped(nums => [...nums, v])}>
+                  <div className={flipped.includes(v) ? '' : 'flipped'}><p>{coupons[v]}</p></div>
+                  <div className={flipped.includes(v) ? 'flipped' : ''}></div>
+                </div>
+              )}
+            </div>
           </>
         }
       </div>
@@ -45,11 +121,3 @@ function App() {
 
 export default App
 
-/*
- * IDEAS
- * Will you be my valentines?
- * Locked before valentines
- * Coupons
- * A Game
- *
-*/
